38d1a35b39a21b8a1836c4ebde80e8dcb94ee29c
Eduard Keilholz <e.keilholz@4dotnet.nl>
2020-10-26 11:18:38 +0100

    Update config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  backendUrl: 'http://localhost:7071',
-  prodbackendUrl: 'https://shortlink-prod-maintenance-app.azurewebsites.net',
+  backendUrl: 'https://shortlink-prod-maintenance-app.azurewebsites.net',
 };
 
 /*
